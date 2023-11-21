19f062e300ca04e8e0350af844ce72fbae24c72d
silvajpedro <silvajpro.31@gmail.com>
2023-11-20 23:23:23 -0300

    testando novo build

--- pages/Components/Characters/Characters.js
+++ pages/Components/Characters/Characters.js
@@ -16,7 +16,7 @@ export default function Characters() {
 
     const getPosts = async () => {
         const response = await axios.get(
-            `https://www.narutodb.xyz/api/character?page=${nextPage}&limit=15`
+            `https://www.narutodb.xyz/api/character?page=${nextPage}&limit=16`
         );
         setPosts(response.data.characters);
     };
